b7d1d695515ed55f903775ad31c13785b8d784ee
nullpointer-01 <223323887+NullPointer-01@users.noreply.github.com>
2026-09-01 22:26:31 +0530

    Add shutdown hook for api server

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"distributed-job-scheduler/internal/config"
 )
@@ -24,23 +25,32 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	startServer(conf)
-
-	<-ctx.Done()
-	slog.Info("Shutting down application")
-}
-
-func startServer(conf config.Config) {
+	// Http Server
 	slog.Info("Starting server on ", "addr", conf.ServerAddr, "port", conf.ServerPort)
+	addr := conf.ServerAddr + ":" + conf.ServerPort
 
-	http.HandleFunc("/", indexHandler)
+	apiMux := http.NewServeMux()
+	apiMux.HandleFunc("/", indexHandler)
+
+	apiServer := &http.Server{
+		Addr:    addr,
+		Handler: apiMux,
+	}
+
 	go func() {
-		addr := conf.ServerAddr + ":" + conf.ServerPort
-		err := http.ListenAndServe(addr, nil)
-		if err != nil {
+		err := apiServer.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
 			slog.Error("Failed to start server", "err", err)
 		}
 	}()
+
+	<-ctx.Done()
+	slog.Info("Shutting down application")
+
+	shutdownCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	apiServer.Shutdown(shutdownCtx)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
