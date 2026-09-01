b56f02fb2112080fa1ef9875fefa160c784d5bb0
nullpointer-01 <223323887+NullPointer-01@users.noreply.github.com>
2026-09-01 22:21:48 +0530

    Implement a http server

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"distributed-job-scheduler/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, Scheduler!")
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
+
+	conf, err := config.Load()
+	if err != nil {
+		slog.Error("Failed to load configuration", "err", err)
+		os.Exit(1)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	startServer(conf)
+
+	<-ctx.Done()
+	slog.Info("Shutting down application")
+}
+
+func startServer(conf config.Config) {
+	slog.Info("Starting server on ", "addr", conf.ServerAddr, "port", conf.ServerPort)
+
+	http.HandleFunc("/", indexHandler)
+	go func() {
+		addr := conf.ServerAddr + ":" + conf.ServerPort
+		err := http.ListenAndServe(addr, nil)
+		if err != nil {
+			slog.Error("Failed to start server", "err", err)
+		}
+	}()
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, Scheduler!")
 }
